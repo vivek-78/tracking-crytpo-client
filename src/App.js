c691d686f5c9d80ab2ef1c264bd4897df419632b
Rahul <rsrsrahul959@gmail.com>
2023-07-09 19:23:06 +0530

    feat: added news and graph components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import CryptoTable from './CryptoTable';
 import "./App.css";
 import Navbar from "./NavBar";
+import CryptoDetail from './CryptoDetail';
+import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <CryptoTable />
+      <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<CryptoTable />} />
+            <Route path=":coin" element={<CryptoDetail />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
  
 export default App;
+
