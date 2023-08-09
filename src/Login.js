5711dca3640d7c0b525e5525ac63de00da4e3f5d
Rahul <rsrsrahul959@gmail.com>
2023-08-10 00:32:42 +0530

    added elements to detail page

--- src/Login.js
+++ src/Login.js
@@ -23,6 +23,7 @@ const Login = () => {
     if (isLoggedIn) {
       navigate("/");
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const handleSumbit = async () => {
     try {
