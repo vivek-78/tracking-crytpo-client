a33859d74ccf3d7153f3561e40d02380531f9602
Rahul <rsrsrahul959@gmail.com>
2023-09-29 22:16:31 +0530

    added add to watchlist to detail page

--- src/CryptoDetail/index.js
+++ src/CryptoDetail/index.js
@@ -1,14 +1,20 @@
 import { React, useState, useEffect } from "react";
 import { Grid, Typography } from "@mui/material";
 import CryptoDetailGraph from "./CryptoDetailGraph";
-import CoinDescription from "./CoinDescription";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
+import { Button } from "@mui/material";
 import { useParams } from "react-router-dom";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { addCoin, removeCoin } from "../store/WatchListSlice";
+
 const CryptoDetail = (props) => {
   const { coin } = useParams();
+  const [isWatchList, setIsWacthList] = useState();
   const [coinData, setCoinData] = useState({});
+  const dispatch = useDispatch();
+  const authToken = useSelector(state=>state.AuthToken.authToken);
   useEffect(() => {
     console.log(coin);
     async function fetchData() {
@@ -22,6 +28,46 @@ const CryptoDetail = (props) => {
     fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+    const fetchWatchList = async () => {
+    try {
+      const fetchedData = await axios.post("http://localhost:8080/watchList", {
+        headers: {
+          authorization: authToken,
+        },
+      });
+      const temp = fetchedData?.data;
+      setIsWacthList(temp.includes(coin));
+    } catch (err) {
+      alert(err);
+    }
+  };
+  useEffect(() => {
+    fetchWatchList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  
+  const handleRemoveCoin = async (coin) => {
+    dispatch(removeCoin({ coin }));
+    await axios.post("http://localhost:8080/removeFromWatchlist", {
+      headers: {
+        authorization: authToken,
+      },
+      coin: coin,
+    });
+    setIsWacthList(!isWatchList);
+  };
+
+  const handleAddCoin = async (coin) => {
+    dispatch(addCoin({ coin }));
+    await axios.post("http://localhost:8080/addToWatchlist", {
+      headers: {
+        authorization: authToken,
+      },
+      coin: coin,
+    });
+    setIsWacthList(!isWatchList);
+  };
   return (
     <Grid
       container
@@ -39,16 +85,39 @@ const CryptoDetail = (props) => {
         sx={{ marginTop: "5px", marginLeft: "10px" }}
         xs={3}
       >
-        <Grid item>
-          <Typography variant="h6">
-            <img
-              src={`https://www.cryptocompare.com${coinData.IMAGEURL}`}
-              width={40}
-              height={40}
-              alt=""
-            />
-            {coin}
-          </Typography>
+        <Grid container justifyContent="space-between" direction="row">
+          <Grid item>
+            <Typography variant="h6">
+              <img
+                src={`https://www.cryptocompare.com${coinData.IMAGEURL}`}
+                width={40}
+                height={40}
+                alt=""
+              />
+              {coin}
+            </Typography>
+          </Grid>
+          <Grid item alignSelf="flex-end">
+            {isWatchList ? (
+              <Button
+                variant="outlined"
+                color={"error"}
+                onClick={() => handleRemoveCoin(coin)}
+                startIcon={<RemoveIcon />}
+              >
+                Watch List
+              </Button>
+            ) : (
+              <Button
+                variant="outlined"
+                color={"success"}
+                onClick={() => handleAddCoin(coin)}
+                startIcon={<AddIcon />}
+              >
+                Watch List
+              </Button>
+            )}{" "}
+          </Grid>
         </Grid>
         <Grid item>
           <Typography variant={"h3"}>{coinData.PRICE}</Typography>
@@ -105,20 +174,3 @@ const InfoComponent = (props) => {
 };
 
 export default CryptoDetail;
-
-{
-  /* <Grid
-        item
-        container
-        direction="row"
-        justifyContent="flex-start"
-        alignItems="flex-start"
-        spacing={5}
-      >
-        <InfoComponent heading="Market Capital" value={coinData.PRICE} />
-        <InfoComponent heading="Volume" value={coinData.TOTALVOLUME24HTO} />
-        <InfoComponent heading="Opening Price" value={coinData.OPEN24HOUR} />
-        <InfoComponent heading="Highest Today" value={coinData.HIGH24HOUR} />
-        <InfoComponent heading="Lowest Today" value={coinData.LOW24HOUR} />
-      </Grid> */
-}
