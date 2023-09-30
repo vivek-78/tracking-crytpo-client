9a732873ddf68d68d6c3c9cfe1ee037fb96d1563
Rahul <rsrsrahul959@gmail.com>
2023-09-30 12:07:53 +0530

    added search feat

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppBar, Toolbar, Typography, Grid, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { removeAuthToken } from "./store/authTokenSlice";
 import { removeWatchList } from "./store/WatchListSlice";
 import { useDispatch,useSelector } from "react-redux";
+import SearchIcon from '@mui/icons-material/Search';
 import { toast } from 'react-toastify';
+import axios from "axios";
+import "./NavBar.css"
 
 function NavBar() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const isLoggedIn = useSelector(state => state.AuthToken.isLoggedIn);
+  const [search, setSearch] = useState("");
+  const textOnChange = (e) => {
+    setSearch(e.target.value);
+  };
+
   const handleClick = ()=>{
      dispatch(removeAuthToken());
      dispatch(removeWatchList());
@@ -17,6 +25,14 @@ function NavBar() {
      toast.success('Logged out Successfully', {
       position: toast.POSITION.TOP_RIGHT
   });
+  }
+    const handleSearchClick = async ()=>{
+    const FetchedData = await axios.get(`https://api.coingecko.com/api/v3/search?query=${search}`);
+    const coinSymbol = FetchedData?.data?.coins[0]?.symbol;
+    const coinName = FetchedData?.data?.coins[0]?.id;
+    if (search.toUpperCase() === coinSymbol || search.toLowerCase() === coinName) {
+      navigate(`/${coinSymbol}`);
+    }
   }
   return (
     <AppBar
@@ -39,6 +55,31 @@ function NavBar() {
               Tracking Crypto
             </Typography>
           </Grid>
+          <form class="search-container">
+            <Grid
+              item 
+              container
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center">
+              <Grid  item>
+                <input
+                  type="text"
+                  id="search-bar"
+                  placeholder="Search for Crypto coins"
+                  onChange={textOnChange}
+                />
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  sx={{ marginLeft: "10px" }}
+                  onClick={() => handleSearchClick()}
+                  startIcon={<SearchIcon fontSize="large" />}
+                >Search</Button>
+              </Grid>
+            </Grid>
+          </form>
           {isLoggedIn && <Grid item>
           <Button variant="outlined" onClick={() => handleClick()}>Log Out</Button>
           </Grid>}
